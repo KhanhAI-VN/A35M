ec4083551588473468a8c6606c13b7a1e795c328
kvu1342009-pixel <kvu1342009@gmail.com>
2026-05-13 01:15:48 +0700

    refactor: introduce retry mechanism for connection establishment and DNS resolution in SSL handshake

--- src/include/openSSL.h
+++ src/include/openSSL.h
@@ -44,67 +44,74 @@ static inline SSLConnection create_ssl_connection(const char *h) {
         SSL_CTX_set_session_cache_mode(ctx, SSL_SESS_CACHE_CLIENT);
         SSL_CTX_sess_set_new_cb(ctx, new_session_cb);
     }
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    struct timeval tv = {5, 0};
-    setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-    setsockopt(s, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
-    struct sockaddr_in a = {AF_INET, htons(443)};
 
-    pthread_mutex_lock(&ssl_mutex);
     int i = strcmp(h, hosts[0]) ? 1 : 0;
-    if (!sessions[i].ip[0] || difftime(time(NULL), sessions[i].ip_t) > 86400) {
-        pthread_mutex_unlock(&ssl_mutex);
-        struct addrinfo hints = {0}, *res; hints.ai_family = AF_INET;
-        if (getaddrinfo(h, NULL, &hints, &res) == 0) {
-            pthread_mutex_lock(&ssl_mutex);
-            inet_ntop(AF_INET, &((struct sockaddr_in *)res->ai_addr)->sin_addr, sessions[i].ip, 16);
-            sessions[i].ip_t = time(NULL);
+
+    for (int retry = 0; retry <= 1; retry++) {
+        int s = socket(AF_INET, SOCK_STREAM, 0);
+        struct timeval tv = {5, 0};
+        setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+        setsockopt(s, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv));
+        struct sockaddr_in a = {AF_INET, htons(443)};
+
+        pthread_mutex_lock(&ssl_mutex);
+        if (!sessions[i].ip[0] || difftime(time(NULL), sessions[i].ip_t) > 86400) {
             pthread_mutex_unlock(&ssl_mutex);
-            freeaddrinfo(res);
-        } else {
+            struct addrinfo hints = {0}, *res; hints.ai_family = AF_INET;
+            if (getaddrinfo(h, NULL, &hints, &res) == 0) {
+                pthread_mutex_lock(&ssl_mutex);
+                inet_ntop(AF_INET, &((struct sockaddr_in *)res->ai_addr)->sin_addr, sessions[i].ip, 16);
+                sessions[i].ip_t = time(NULL);
+                pthread_mutex_unlock(&ssl_mutex);
+                freeaddrinfo(res);
+            } else {
+                pthread_mutex_lock(&ssl_mutex);
+                if (!sessions[i].ip[0]) { pthread_mutex_unlock(&ssl_mutex); close(s); return (SSLConnection){0}; }
+                pthread_mutex_unlock(&ssl_mutex);
+            }
             pthread_mutex_lock(&ssl_mutex);
-            if (!sessions[i].ip[0]) { pthread_mutex_unlock(&ssl_mutex); return close(s), (SSLConnection){0}; }
+        }
+        struct in_addr ia; inet_pton(AF_INET, sessions[i].ip, &ia);
+        memcpy(&a.sin_addr, &ia, sizeof(ia));
+
+        SSL_SESSION *sess = sessions[i].s;
+        if (sess && difftime(time(NULL), sessions[i].t) > 86400) sess = NULL;
+        pthread_mutex_unlock(&ssl_mutex);
+
+        if (connect(s, (struct sockaddr*)&a, sizeof(a)) < 0) {
+            close(s);
+            pthread_mutex_lock(&ssl_mutex);
+            sessions[i].ip[0] = 0;  /* invalidate cache, force re-resolve on next attempt */
+            pthread_mutex_unlock(&ssl_mutex);
+            continue;  /* retry: re-resolve DNS then reconnect */
+        }
+
+        SSL *ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, s);
+        SSL_set_tlsext_host_name(ssl, h);
+        if (sess) SSL_set_session(ssl, sess);
+
+        int r = SSL_connect(ssl);
+        if (r <= 0) {
+            SSL_free(ssl);
+            pthread_mutex_lock(&ssl_mutex);
+            if (sessions[i].s) { SSL_SESSION_free(sessions[i].s); sessions[i].s = NULL; }
+            pthread_mutex_unlock(&ssl_mutex);
+            ssl = SSL_new(ctx); SSL_set_fd(ssl, s); SSL_set_tlsext_host_name(ssl, h);
+            r = SSL_connect(ssl);
+            if (r <= 0) { SSL_free(ssl); close(s); return (SSLConnection){0}; }
+        }
+
+        if (SSL_session_reused(ssl)) {
+            pthread_mutex_lock(&ssl_mutex);
+            sessions[i].t = time(NULL);
             pthread_mutex_unlock(&ssl_mutex);
         }
-        pthread_mutex_lock(&ssl_mutex);
-    }
-    struct in_addr ia; inet_pton(AF_INET, sessions[i].ip, &ia);
-    memcpy(&a.sin_addr, &ia, sizeof(ia));
-    
-    SSL_SESSION *sess = sessions[i].s;
-    if (sess && difftime(time(NULL), sessions[i].t) > 86400) sess = NULL;
-    pthread_mutex_unlock(&ssl_mutex);
 
-    if (connect(s, (struct sockaddr*)&a, sizeof(a)) < 0) {
-        pthread_mutex_lock(&ssl_mutex);
-        sessions[i].ip[0] = 0;
-        pthread_mutex_unlock(&ssl_mutex);
-        return close(s), (SSLConnection){0};
+        return (SSLConnection){ssl, s};
     }
 
-    SSL *ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, s);
-    SSL_set_tlsext_host_name(ssl, h);
-    if (sess) SSL_set_session(ssl, sess);
-
-    int r = SSL_connect(ssl);
-    if (r <= 0) {
-        SSL_free(ssl);
-        pthread_mutex_lock(&ssl_mutex);
-        if (sessions[i].s) { SSL_SESSION_free(sessions[i].s); sessions[i].s = NULL; }
-        pthread_mutex_unlock(&ssl_mutex);
-        ssl = SSL_new(ctx); SSL_set_fd(ssl, s); SSL_set_tlsext_host_name(ssl, h);
-        r = SSL_connect(ssl);
-        if (r <= 0) { close(s); return (SSLConnection){0}; }
-    }
-
-    if (SSL_session_reused(ssl)) {
-        pthread_mutex_lock(&ssl_mutex);
-        sessions[i].t = time(NULL);
-        pthread_mutex_unlock(&ssl_mutex);
-    }
-
-    return (SSLConnection){ssl, s};
+    return (SSLConnection){0};
 }
 
 
